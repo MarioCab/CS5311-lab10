2f0f8bb623b836b1c4e823a63daf2830019499b1
Mario Cabrera <yankeescab@gmail.com>
2024-10-31 18:43:05 -0400

    Finished part 2

--- js/pricing.js
+++ js/pricing.js
@@ -8,6 +8,11 @@ document
   .getElementById("num-guests")
   .addEventListener("focusout", validateGuestInput);
 
+// Event listener to check input for valid number of nights.
+document
+  .getElementById("num-nights")
+  .addEventListener("focusout", validateNightInput);
+
 /**
  * Checks to see if the user input is valid, based on the result, different elements are displayed
  */
@@ -19,6 +24,18 @@ function validateGuestInput() {
     guestsError.classList.remove("display-none");
   } else {
     guestsError.classList.add("display-none");
+    return true;
+  }
+}
+
+function validateNightInput() {
+  let numNights = document.getElementById("num-nights").value;
+  let nightsError = document.getElementById("nights-error");
+
+  if (Number.parseInt(numNights) < 2 || numNights === "") {
+    nightsError.classList.remove("display-none");
+  } else {
+    nightsError.classList.add("display-none");
     return true;
   }
 }
