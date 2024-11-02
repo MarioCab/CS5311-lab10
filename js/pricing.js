67855415c0d4f68096d148323b42eaef44100452
Mario Cabrera <yankeescab@gmail.com>
2024-11-01 20:33:06 -0400

    Finished calculating cost function

--- js/pricing.js
+++ js/pricing.js
@@ -13,6 +13,11 @@ document
   .getElementById("num-nights")
   .addEventListener("focusout", validateNightInput);
 
+// Event listener to check input for the cost field.
+document
+  .getElementById("price-button")
+  .addEventListener("click", calculateCosts);
+
 /**
  * Checks to see if the user input for guests is valid, based on the result, different elements are displayed
  */
@@ -45,25 +50,59 @@ function validateNightInput() {
   }
 }
 
+/**
+ * Checks to see if the nights and the guests input are both valid
+ */
 function validateAllInputs() {
   let priceResult = document.getElementById("price-result").value;
 
-  if (validateNightInput == false || validateGuestInput == false) {
+  if (validateNightInput() == false || validateGuestInput() == false) {
     priceResult.classList.add("display-none");
     return;
   } else return true;
 }
 
+/**
+ * Checks if dinner is added for the guests
+ */
+function isDinnerAdded() {
+  let isDinnerChecked = document.getElementById("dinner").checked;
+
+  if (isDinnerChecked) {
+    return Number.parseInt(document.getElementById("num-guests").value);
+  } else return 0;
+}
+
+/**
+ * Checks if breakfast is added for the guests
+ */
+function isBreakfastAdded() {
+  let isBreakfastChecked = document.getElementById("breakfast").checked;
+
+  if (isBreakfastChecked) {
+    return Number.parseInt(document.getElementById("num-guests").value);
+  } else return 0;
+}
+
+/**
+ * Calculates the total costs of the stay
+ */
 function calculateCosts() {
-  let x = Number.parseInt(numGuests);
-  let personPerNight = x * 30;
-  let breakfastPerPerson = x * 10;
-  let dinnerPerPerson = x * 20;
+  let numGuests = document.getElementById("num-guests").value;
+  let personPerNight = Number.parseInt(numGuests) * 30;
+  let breakfastPerPerson = isBreakfastAdded() * 10;
+  let dinnerPerPerson = isDinnerAdded() * 20;
   let preTaxTotal = personPerNight + breakfastPerPerson + dinnerPerPerson;
   let taxes = preTaxTotal * 0.21;
   let total = preTaxTotal + taxes;
 
-  while (validateAllInputs() == true) {
-    
+  if (validateAllInputs() == true) {
+    document.getElementById("price-value").innerHTML = preTaxTotal.toFixed(2);
+
+    document.getElementById("taxes-value").innerHTML = taxes.toFixed(2);
+
+    document.getElementById("total-value").innerHTML = total.toFixed(2);
+
+    document.getElementById("price-result").classList.remove("display-none");
   }
 }
